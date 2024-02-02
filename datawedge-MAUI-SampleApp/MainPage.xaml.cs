0899427913e1a2e9dc276a7a7f7504cb7cb895d9
N.DZL <cxnt48@gmail.com>
2024-02-02 15:27:36 +0100

    Switchin OFF/ON Datawedge via intent APIs
    Reference https://techdocs.zebra.com/datawedge/latest/guide/api/scannerinputplugin/

--- datawedge-MAUI-SampleApp/MainPage.xaml.cs
+++ datawedge-MAUI-SampleApp/MainPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
+using Java.Lang;
+using System.Security.AccessControl;
+using System.Xml.Linq;
 
 namespace datawedge_MAUI_SampleApp;
 
@@ -12,7 +15,8 @@ public partial class MainPage : ContentPage
 
 		WeakReferenceMessenger.Default.Register<string>(this, (r, m) =>
 		{
-			MainThread.BeginInvokeOnMainThread(() => { lbDisplayBarcodeData.Text += "\n"+m; });
+			if(m.Length>2)
+				MainThread.BeginInvokeOnMainThread(() => { lbDisplayBarcodeData.Text += "\n" + m; });
 		});
 	}
 
@@ -27,5 +31,17 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    private void OnDWOFFClicked(object sender, EventArgs e)
+    {
+        WeakReferenceMessenger.Default.Send("11");
+        WeakReferenceMessenger.Default.Send("SWITCHING OFF DW");
+    }
+
+    private void OnDWONClicked(object sender, EventArgs e)
+    {
+        WeakReferenceMessenger.Default.Send("22");
+        WeakReferenceMessenger.Default.Send("SWITCHING ON DW");
+    }
 }
 
--- datawedge-MAUI-SampleApp/Platforms/Android/MainActivity.cs
+++ datawedge-MAUI-SampleApp/Platforms/Android/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.OS;
 using Android.Util;
 using CommunityToolkit.Mvvm.Messaging;
 using datawedge_MAUI_SampleApp.Platforms.Android;
+using Java.Lang;
 using System.Diagnostics.Metrics;
 using System.Security.AccessControl;
 using System.Xml.Linq;
@@ -15,17 +16,47 @@ namespace datawedge_MAUI_SampleApp;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true)]
 public class MainActivity : MauiAppCompatActivity
 {
-	protected override void OnCreate(Bundle savedInstanceState) {
-        base.OnCreate(savedInstanceState);
+    //protected override void OnCreate(Bundle savedInstanceState) {
+    protected override void OnPostCreate(Bundle savedInstanceState)
+    { 
+        base.OnPostCreate(savedInstanceState);
         RegisterReceivers();
 
+
+
+
+            WeakReferenceMessenger.Default.Register<string>(this, (r, li) =>
+            {
+                MainThread.BeginInvokeOnMainThread(() => {
+                    if (li == "11")
+                    {
+                        Intent i = new Intent();
+                        i.SetAction("com.symbol.datawedge.api.ACTION");
+                        i.PutExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN");
+                        i.PutExtra("SEND_RESULT", "true");
+                        i.PutExtra("COMMAND_IDENTIFIER", "MY_DISABLE_SCANNER");  //Unique identifier
+                        this.SendBroadcast(i);
+                    }
+                    else if (li=="22") {
+                        Intent i = new Intent();
+                        i.SetAction("com.symbol.datawedge.api.ACTION");
+                        i.PutExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "ENABLE_PLUGIN");
+                        i.PutExtra("SEND_RESULT", "true");
+                        i.PutExtra("COMMAND_IDENTIFIER", "MY_ENABLE_SCANNER");  //Unique identifier
+                        this.SendBroadcast(i);
+                    }
+
+                });
+            });
         //showing saved states 
         try
         {
-            String savedDatetime = savedInstanceState.GetString("time");
+            string savedDatetime = savedInstanceState.GetString("time");
             if (savedDatetime is not null)
                 WeakReferenceMessenger.Default.Send("Saved DateTime=" + savedDatetime);
-        }catch(Exception ex) {
+
+        }
+        catch(System.Exception ex) {
             WeakReferenceMessenger.Default.Send("No previously saved instance available");
         }
     }
@@ -33,7 +64,7 @@ public class MainActivity : MauiAppCompatActivity
 
     protected override void OnSaveInstanceState(Bundle outState)
     {
-        String currentDatetime = DateTime.Now.ToString();
+        string currentDatetime = DateTime.Now.ToString();
         outState.PutString("time", currentDatetime);
         base.OnSaveInstanceState(outState);
     }
@@ -43,6 +74,7 @@ public class MainActivity : MauiAppCompatActivity
 		IntentFilter filter = new IntentFilter();
 		filter.AddCategory("android.intent.category.DEFAULT");
 		filter.AddAction("com.ndzl.DW");
+		filter.AddAction("com.zebra.sensors");
 
 		Intent regres = RegisterReceiver(new DWIntentReceiver(), filter);
 	}
